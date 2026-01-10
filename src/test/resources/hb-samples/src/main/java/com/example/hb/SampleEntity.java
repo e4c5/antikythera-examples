c242e1bbf6abc45dbfd0916bb6302fe83d64887b
raditha dissanayake <raditha@raditha.com>
2026-01-10 11:29:14 +0530

    Nothing big here

--- src/test/resources/hb-samples/src/main/java/com/example/hb/SampleEntity.java
+++ src/test/resources/hb-samples/src/main/java/com/example/hb/SampleEntity.java
@@ -1,22 +1,34 @@
 package com.example.hb;
 
 import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.Type;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Convert;
+import com.example.hb.converters.JsonbAttributeConverter;
 
 @TypeDef(name = "jsonb", typeClass = java.lang.Object.class)
 @Entity
 public class SampleEntity {
+
     @Id
     private Long id;
 
-    @Type(type = "jsonb")
+    @Convert(converter = JsonbAttributeConverter.class)
     private Object data;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public Object getData() { return data; }
-    public void setData(Object data) { this.data = data; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
